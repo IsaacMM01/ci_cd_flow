20a777e2aff8b13f031221cac3203e3f9525b888
IsaacMM <maresmezaisaac1@gmail.com>
2025-09-20 15:15:07 -0600

    test: probando funcionalidad de despliegue #1

--- app/go_app.go
+++ app/go_app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hola desde Go")
+		fmt.Fprintf(w, "Hola desde mi proyecto")
 	})
 
 	http.HandleFunc("/saludo", func(w http.ResponseWriter, r *http.Request) {
