2de9fbdb5b7f9031e618a78288f1545214054a46
MagdalenoGarcia <magdaleno.garcia@fraternitas.io>
2025-09-23 10:06:13 -0600

    test case 1

--- app/go_app.go
+++ app/go_app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hola desde mi proyecto")
+		fmt.Fprintf(w, "Hola desde mi proyecto, test case 1")
 	})
 
 	http.HandleFunc("/saludo", func(w http.ResponseWriter, r *http.Request) {
